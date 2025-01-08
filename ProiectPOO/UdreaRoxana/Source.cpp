5d68144ee6cd4aff31326521069fb8b1b180ed4f
RoxanaU <udrearoxana12@stud.ase.ro>
2025-01-09 00:09:08 +0200

    Faza 2

--- ProiectPOO/UdreaRoxana/Source.cpp
+++ ProiectPOO/UdreaRoxana/Source.cpp
@@ -4,107 +4,160 @@
 
 using namespace std;
 
-//Proiect - Faza 1
-//Roxana Maria Udrea, clase alese Restaurant, Meniu, Uniforme
-
 class Restaurant {
-public:
+private:
     string* nume;
     int capacitate;
     static int numarRestaurante;
     const int idRestaurant;
 
-    Restaurant(string nume, int capacitate) : idRestaurant(1), capacitate(capacitate) {
+public:
+    // Constructori
+    Restaurant(string nume, int capacitate) : idRestaurant(numarRestaurante + 1), capacitate(capacitate) {
         this->nume = new string(nume);
         numarRestaurante++;
     }
-    Restaurant(string nume) : idRestaurant(2), capacitate(50) {
-        this->nume = new string(nume);
-        numarRestaurante++;
-    }
-    Restaurant() : nume(new string("Nedefinit")), capacitate(0), idRestaurant(3) {
-        numarRestaurante++;
+    Restaurant(const Restaurant& src) : idRestaurant(src.idRestaurant), capacitate(src.capacitate) {
+        nume = new string(*src.nume);
     }
 
+    // Destructor
     ~Restaurant() {
         delete nume;
     }
 
-    static void afiseazaNumarRestaurante() {
-        cout << "Numarul total de restaurante: " << numarRestaurante << endl;
+    // Metode de acces
+    string getNume() const {
+        return *nume;
     }
+    void setNume(string nouNume) {
+        *nume = nouNume;
+    }
+    int getCapacitate() const {
+        return capacitate;
+    }
+    void setCapacitate(int nouaCapacitate) {
+        capacitate = nouaCapacitate;
+    }
+
+    // Metoda statica
+    static int getNumarRestaurante() {
+        return numarRestaurante;
+    }
+
+    // Functia prieten (exemplu)
+    friend void afiseazaDetaliiRestaurant(const Restaurant& r);
 };
 
 int Restaurant::numarRestaurante = 0;
 
+void afiseazaDetaliiRestaurant(const Restaurant& r) {
+    cout << "Restaurantul " << *r.nume << " are o capacitate de " << r.capacitate << " persoane." << endl;
+}
+
 class Meniu {
-public:
+private:
     string* tip;
     vector<string> listaPreparate;
     static int numarMeniuri;
     const int idMeniu;
 
-    Meniu(string tip, vector<string> listaPreparate) : idMeniu(101), listaPreparate(listaPreparate) {
+public:
+    Meniu(string tip, vector<string> listaPreparate) : idMeniu(numarMeniuri + 1), listaPreparate(listaPreparate) {
         this->tip = new string(tip);
         numarMeniuri++;
     }
-    Meniu(string tip) : idMeniu(102), listaPreparate({ "Fel principal", "Desert" }) {
-        this->tip = new string(tip);
-        numarMeniuri++;
-    }
-    Meniu() : tip(new string("General")), listaPreparate({ "Băutură", "Aperitiv", "Desert" }), idMeniu(103) {
-        numarMeniuri++;
+    Meniu(const Meniu& src) : idMeniu(src.idMeniu), listaPreparate(src.listaPreparate) {
+        tip = new string(*src.tip);
     }
 
     ~Meniu() {
         delete tip;
     }
 
-    static void afiseazaNumarMeniuri() {
-        cout << "Numarul total de meniuri: " << numarMeniuri << endl;
+    string getTip() const {
+        return *tip;
     }
+    void setTip(string nouTip) {
+        *tip = nouTip;
+    }
+
+    static int getNumarMeniuri() {
+        return numarMeniuri;
+    }
+
+    friend void printMeniu(const Meniu& m);
 };
 
 int Meniu::numarMeniuri = 0;
 
+void printMeniu(const Meniu& m) {
+    cout << "Meniul " << *m.tip << " contine: ";
+    for (auto& preparat : m.listaPreparate) {
+        cout << preparat << ", ";
+    }
+    cout << endl;
+}
+
 class Uniforma {
-public:
+private:
     string* model;
     int marime;
     static int numarUniforme;
     const int idUniforma;
 
-    Uniforma(string model, int marime) : idUniforma(201), marime(marime) {
+public:
+    Uniforma(string model, int marime) : idUniforma(numarUniforme + 1), marime(marime) {
         this->model = new string(model);
         numarUniforme++;
     }
-    Uniforma(string model) : idUniforma(202), marime(50) {
-        this->model = new string(model);
-        numarUniforme++;
-    }
-    Uniforma() : model(new string("Standard")), marime(42), idUniforma(203) {
-        numarUniforme++;
+    Uniforma(const Uniforma& src) : idUniforma(src.idUniforma), marime(src.marime) {
+        model = new string(*src.model);
     }
 
     ~Uniforma() {
         delete model;
     }
 
-    static void afiseazaNumarUniforme() {
-        cout << "Numarul total de uniforme: " << numarUniforme << endl;
+    string getModel() const {
+        return *model;
     }
+    void setModel(string nouModel) {
+        *model = nouModel;
+    }
+
+    int getMarime() const {
+        return marime;
+    }
+    void setMarime(int nouaMarime) {
+        marime = nouaMarime;
+    }
+
+    static int getNumarUniforme() {
+        return numarUniforme;
+    }
+
+    friend void printUniforma(const Uniforma& u);
 };
 
 int Uniforma::numarUniforme = 0;
 
-int main() {
-    Restaurant r1("La Bucurie", 100), r2("Gustos"), r3;
-    Meniu m1("Vegetarian", { "Salata", "Supa" }), m2("Carnivor"), m3;
-    Uniforma u1("Casual", 36), u2("Elegant"), u3;
+void printUniforma(const Uniforma& u) {
+    cout << "Uniforma modelul " << *u.model << " are marimea " << u.marime << endl;
+}
 
-    Restaurant::afiseazaNumarRestaurante();
-    Meniu::afiseazaNumarMeniuri();
-    Uniforma::afiseazaNumarUniforme();
+int main() {
+    Restaurant r1("La Placinte", 100);
+    Meniu m1("Vegetarian", { "Salata", "Supa de legume" });
+    Uniforma u1("Casual", 40);
+
+    afiseazaDetaliiRestaurant(r1);
+    printMeniu(m1);
+    printUniforma(u1);
+
+    cout << "Numar restaurante: " << Restaurant::getNumarRestaurante() << endl;
+    cout << "Numar meniuri: " << Meniu::getNumarMeniuri() << endl;
+    cout << "Numar uniforme: " << Uniforma::getNumarUniforme() << endl;
 
     return 0;
 }
